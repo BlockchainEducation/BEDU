3634121f3bba7f072c76f04f0a6602a644548d27
beuoon <beuoon@naver.com>
2020-08-25 22:52:24 +0900

    Fix: database package 수정
    Database class가 bucket을 non-static으로 갖게 수정
    Cursor class와 Bucket class의 함수명 및 순서 수정
    데이터 저장 형식을 byte[]에서 Object로 변경

--- src/main/java/database/Cursor.java
+++ src/main/java/database/Cursor.java
@@ -1,41 +1,38 @@
 package database;
 
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.Map;
 import util.Pair;
 
 public class Cursor {
-    ConcurrentHashMap<String, byte[]> db;
-    ArrayList<String> keys;
-    int idx = -1;
+    private final Map<String, Object> db;
+    private final List<String> keys = new ArrayList<>();
+    private int idx = -1;
 
-    public Cursor(ConcurrentHashMap<String, byte[]> db) {
+    public Cursor(Map<String, Object> db) {
         this.db = db;
-        keys = new ArrayList<String>();
         keys.addAll(db.keySet());
     }
 
-    public Pair<String, byte[]> first() {
+    public Pair<String, Object> head() {
         idx = 0;
         return new Pair<>(keys.get(idx), db.get(keys.get(idx)));
     }
-
-    public Boolean hasNext() {
-        if(idx+1 >= keys.size()) return false;
-        return true;
-    }
-
-    public Pair<String, byte[]> tail() {
-        idx = db.size()-1;
+    public Pair<String, Object> tail() {
+        idx = keys.size()-1;
         return new Pair<>(keys.get(idx), db.get(keys.get(idx)));
     }
 
-    public Pair<String, byte[]> get(int idx){
-        return new Pair<>(keys.get(idx), db.get(keys.get(idx)));
-    }
-
-    public Pair<String, byte[]> next() {
+    public Pair<String, Object> next() {
         idx++;
+        return new Pair<>(keys.get(idx), db.get(keys.get(idx)));
+    }
+    public Boolean hasNext() {
+        return idx + 1 < keys.size();
+    }
+
+    public Pair<String, Object> get(int idx) {
         return new Pair<>(keys.get(idx), db.get(keys.get(idx)));
     }
 }
--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,14 +1,15 @@
 package database;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Database {
-    private static ConcurrentHashMap<String, Bucket> bucket = new ConcurrentHashMap();
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
-    public static Bucket getBucket(String bName) {
-        Bucket b = bucket.get(bName);
+    public Bucket getBucket(String bName) {
+        Bucket b = buckets.get(bName);
         if(b == null) {
-            bucket.put(bName, new Bucket());
+            buckets.put(bName, new Bucket());
         }
         return b;
     }
