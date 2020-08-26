941f75fed1a9155b0802fc3391fe3b744c9451b0
beuoon <beuoon@naver.com>
2020-08-26 20:19:37 +0900

    Add: node package 구현
    Node class 구현

--- src/main/java/node/Node.java
+++ src/main/java/node/Node.java
@@ -1,4 +1,34 @@
 package node;
 
-public class Node {
+import network.Network;
+import network.NetworkListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Node implements NetworkListener {
+    protected final String nodeId;
+    protected final List<String> connections = new ArrayList<>();
+
+    public Node(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public void open() {
+        Network.AddListener(nodeId, this);
+    }
+    public void close() {
+        Network.RemoveListener(nodeId);
+    }
+
+    public void connectTo(String to) {
+        connections.add(to);
+    }
+    public void disconnectTo(String to) {
+        connections.remove(to);
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
 }
